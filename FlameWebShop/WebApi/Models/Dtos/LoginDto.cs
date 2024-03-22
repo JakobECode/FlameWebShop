2f6a2f4e794ec08f320507f0ce88820eb69ddbe0
Yakob Eliyo <jakob.darios@gmail.com>
2024-03-22 08:57:39 +0100

    changes in

---
+++ FlameWebShop/WebApi/Models/Dtos/LoginDto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Dtos
+{
+    public class LoginDto
+    {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = null!;
+        [Required]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$", ErrorMessage = "Password is too weak. Include at least one number and one special sign")]
+        public string Password { get; set; } = null!;
+
+        public static implicit operator IdentityUser(LoginDto model)
+        {
+            return new IdentityUser()
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+        }
+    }
+}
